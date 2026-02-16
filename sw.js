0afd00fa1308bce88e619304afd4eeace3a220be
todi3103-max <todi3103@gmail.com>
2026-02-16 21:44:31 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "testsim-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
